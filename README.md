496eab9e0a6c6daed7892d1885d5ac6718fc2b6d
Lugieda <florian.hulke@gmail.com>
2023-10-06 15:09:59 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # SpigotWorldExtractor
-Batch files for bulk-extracting the world of spigot / paper servers
+
+Batch files for bulk-extracting the world of spigot / paper servers.
+
+# How to use
+
+You can drag and drop multiple server folders over the batch file. The extracted world folder gets copied to a specific destination folder. The destination folder is different in each batch file. This is the only difference between them.
+
+There are three different batch files provided:
+
+1. SpigotWorldExtractor_To_Minecraft: Copies the extracted world to %appdata%/.minecraft/saves
+2. SpigotWorldExtractor_To_Source: Copies the extracted world to the same folder, where the server folder is located
+3. SpigotWorldExtractor_To_Here: Copies the extracted folder to the same folder, where the batch file is located
+
+# What does the script?
+
+The scripts functionality implements the [migration guidelines on how to migrate from a paper server to a vanilla one](https://docs.papermc.io/paper/migration#migrating-from-paper).
---
+++ SpigotWorldExtractor_To_Minecraft.bat
@@ -0,0 +1,55 @@
+@echo off
+
+if [%1]==[] goto PATH_NOT_PROVIDED
+set baseOutputPath=%~dp0
+
+:LOOP
+for %%a in (%1) do set outputDir=%appdata%\.minecraft\saves\%%~nxa_world\
+echo Source is "%1"
+echo Destination is "%outputDir%"
+echo.
+
+
+:OVERWORLD
+if not exist "%~1\world" goto OVERWORLD_NOT_FOUND
+echo Copying "overworld" folder recursively 
+robocopy "%~1\world" "%outputDir%" /S > nul
+
+:NETHER
+if not exist "%~1\world_nether\DIM-1\" goto NETHER_NOT_FOUND
+echo Copying "nether" folder recursively
+robocopy "%~1/world_nether/DIM-1/" "%outputDir%\DIM-1" /S > nul
+
+:THE_END
+if not exist "%~1\world_the_end\DIM1\" goto THE_END_NOT_FOUND
+echo Copying "the end" folder recursively
+robocopy "%~1\world_the_end\DIM1\" "%outputDir%\DIM1" /S > nul
+echo.
+
+
+:END_OF_LOOP
+shift
+if not [%1]==[] goto LOOP
+echo DONE. You can close this window now
+goto EOF
+
+:PATH_NOT_PROVIDED
+echo No destination path provided. You can simply drag the parent folder of the paper world over this .bat file! 
+echo HINT: The parent folder is most commonly the paper server folder
+goto EOF
+
+:OVERWORLD_NOT_FOUND
+echo Folder "overworld" not found
+goto END_OF_LOOP
+
+:NETHER_NOT_FOUND
+echo Folder "nether" not found, skipping
+goto THE_END
+
+:THE_END_NOT_FOUND
+echo Folder "the end" not found, skipping
+goto END_OF_LOOP
+
+:EOF
+echo.
+pause
